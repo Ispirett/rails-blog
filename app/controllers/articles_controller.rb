be846d221a2d906d902ccf989a17cfa377d10f55
Ispire <ispirett@gmail.com>
2019-05-22 20:30:16 -0400

    feat(crud): added crud functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,4 +11,34 @@ class ArticlesController < ApplicationController
     @article = Article.new
 
   end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to '/articles'
+    #redirect_to_article_path(@article) if @article.save
+  end
+
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+      @article = Article.find(params[:id])
+      @article = Article.update(article_params)
+      redirect_to articles_path(@article)
+  end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to '/articles'
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
