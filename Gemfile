dbb22f28c697db18b806fc461ccbe5de262cdcff
Ispire <ispirett@gmail.com>
2019-05-24 16:31:53 -0400

    feat(tags): add tags to blog

--- Gemfile
+++ Gemfile
@@ -34,6 +34,9 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
+# templates
+
+
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
@@ -62,3 +65,7 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+group :production do
+  gem 'pg'
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,19 @@
 class Article < ApplicationRecord
-  has_many :comments
+  has_many :comments , dependent: :destroy
+  has_many :taggings , dependent: :destroy
+  has_many :tags , through: :taggings , dependent: :destroy
+
+  def tag_list
+    self.tags.collect do
+
+    |tags| tags.name end.join(",")
+  end
+
+  def tag_list=(tag_string)
+    entered_tags = tag_string.split(",").collect { |t| t.strip.downcase }.uniq
+    new_tags = entered_tags.collect { |tags| Tag.find_or_create_by(name: tags) }
+    self.tags = new_tags
+    
+  end
+
 end
